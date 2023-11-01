204a0c3c08ab52967e1195662f08c00cbf151a49
Bernie Forbes <besanegames@gmail.com>
2023-11-01 15:51:03 +1000

    changes

--- ch_16/threads/src/main.rs
+++ ch_16/threads/src/main.rs
@@ -2,7 +2,7 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let handle = thread::spawn(|| {
+    thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {} from the spawned thread!", i);
             thread::sleep(Duration::from_millis(1));
@@ -13,6 +13,4 @@ fn main() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
-
-    handle.join().unwrap();
 }
