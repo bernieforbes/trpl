38675a78eb38409c17f4f5eeae489192293d2725
Bernie Forbes <besanegames@gmail.com>
2023-11-05 20:08:57 +1000

    changes

--- ch_19/unsafe-rust/src/main.rs
+++ ch_19/unsafe-rust/src/main.rs
@@ -1,8 +1,9 @@
+extern "C" {
+    fn abs(input: i32) -> i32;
+}
+
 fn main() {
-    use std::slice;
-
-    let address = 0x01234usize;
-    let r = address as *mut i32;
-
-    let values: &[i32] = unsafe { slice::from_raw_parts_mut(r, 10000) };
+    unsafe {
+        println!("Absolute value of -3 according to C: {}", abs(-3));
+    }
 }
