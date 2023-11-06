7aba65acc6d8411652807eef153b57e341159b6b
Bernie Forbes <besanegames@gmail.com>
2023-11-06 15:36:42 +1000

    changes

--- ch_19/advanced-traits/src/main.rs
+++ ch_19/advanced-traits/src/main.rs
@@ -1,4 +1,14 @@
+use std::fmt;
+
+struct Wrapper(Vec<String>);
+
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{}]", self.0.join(", "))
+    }
+}
+
 fn main() {
-    let list_of_numbers = vec![1, 2, 3];
-    let list_of_strings: Vec<String> = list_of_numbers.iter().map(ToString::to_string).collect();
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    println!("w = {}", w);
 }
